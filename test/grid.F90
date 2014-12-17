143c7f7112eaeea9a5d2938546f6440ded26613c
jamroz <benjamroz@gmail.com>
2014-12-16 19:01:30 -0500

    Have the pack and unpack cycles running. Pulled out the communication schedule into grid.F90. Need to add the messaging.

--- test/grid.F90
+++ test/grid.F90
@@ -11,6 +11,8 @@ module grid_data
   integer :: gridComm
   integer :: gridRank
   integer :: gridNumProcs
+  integer, target :: startIndex(3), endIndex(3)
+  integer :: factors(3)
 
   real*8 :: hx = -1
   real*8 :: hy = -1
@@ -25,6 +27,9 @@ module grid_data
   public :: read_grid_data_namelist
   public :: grid_3d_decomp_num_elements
   public :: grid_3d_decomp_get_elements
+  public :: grid_3d_get_indices
+  public :: getElemRank
+  public :: determineCommSchedule
 
 contains
 
@@ -131,11 +136,11 @@ contains
   implicit none
   integer, intent(in) :: rank, numProcs
 
-  integer :: factors(3), remainder
+  integer :: remainder
   integer :: cellsInDir(3), localCellsInDir
   integer :: iDir, iRank
-  integer :: startIndex(3), endIndex(3)
   integer :: ierr
+  integer :: counter
 
 
   ! Determine the domain size 
@@ -160,6 +165,7 @@ contains
 
   remainder = numLocalElems
   factors(:) = 1
+  counter = 0
   do while (.true.)
     do iDir = 1,3
       if (mod(remainder, 2) .eq. 0) then
@@ -170,11 +176,17 @@ contains
     if (remainder .eq. 1) then
       exit 
     endif
+    counter = counter + 1
+    if (counter .gt. 100) then
+      print *, "Error: nx,ny,nz must be factors of 2"
+      exit
+    endif
   enddo
 
   if (rank .eq. 0) then
     print *, "factors = ", factors
   endif
+
 
   startIndex(1) = 1
   startIndex(2) = 1
@@ -209,13 +221,192 @@ contains
   end subroutine grid_3d_decomp_num_elements
 
 
-  subroutine grid_3d_decomp_get_elements(rank, numProcs, gridIndices)
+  subroutine grid_3d_get_indices(numElems, gridIndicesStart, gridIndicesEnd)
+  implicit none
+  integer, intent(out) :: numElems
+  integer, pointer, intent(out) :: gridIndicesStart(:), gridIndicesEnd(:)
+
+  numElems = numLocalElems
+  gridIndicesStart => startIndex
+  gridIndicesEnd => endIndex
+
+  end subroutine 
+
+  function getElemRank(ix, iy, iz) result(iRank)
+  implicit none
+  integer, intent(in) :: ix, iy, iz
+  integer :: iRank
+
+  integer :: facX, facY, facZ
+
+  if (ix < 1 .or. ix > nx .or. &
+      iy < 1 .or. iy > ny .or. &
+      iz < 1 .or. iz > nz) then
+    iRank = -1
+  else
+    facX = (ix-1)/factors(1)
+    facY = (iy-1)/factors(2)
+    facZ = (iz-1)/factors(3)
+
+    iRank = facX + nx/factors(1)*facY + nx/factors(1)*ny/factors(2)*facZ
+  endif
+
+  end function
+
+  subroutine determineCommSchedule(rank, numProcs, gridIndicesStart, gridIndicesEnd, &
+                                   numMessages, msgIndices, msgSizes, msgOffsets)
   implicit none
   integer, intent(in) :: rank, numProcs
-  integer, intent(out) :: gridIndices(:,:)
+  integer, intent(in) :: gridIndicesStart(3), gridIndicesEnd(3)
 
+  integer, intent(out) :: numMessages
+  integer, intent(out) :: msgIndices(-1:1,-1:1,-1:1)
+  integer, allocatable, intent(out) :: msgSizes(:), msgOffsets(:)
 
-  end subroutine grid_3d_decomp_get_elements
+  integer :: iRank, ix, iy, iz, elemRank, rankCounter
+  integer :: ierr
+  integer :: elemRankCounter(0:numProcs-1)
+  integer, allocatable :: neighborRanks(:)
+  integer :: tmpDir(-1:1,-1:1,-1:1)
+  integer :: tmpDirX, tmpDirY, tmpDirZ
+  integer :: totalMessageSize, i
+  logical :: found
+
+  do iRank = 0, numProcs - 1
+    if (iRank == rank) then
+      print *, "Rank: start ", gridIndicesStart, ", end ", gridIndicesEnd
+      do iz = gridIndicesStart(3), gridIndicesEnd(3)
+        do iy = gridIndicesStart(2), gridIndicesEnd(2)
+          do ix = gridIndicesStart(1), gridIndicesEnd(1)
+            elemRank = getElemRank(ix,iy,iz)
+            print *, "  Element (", ix, ",", iy, ",", iz, ") belongs to rank ", elemRank
+          enddo
+        enddo
+      enddo
+    endif
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call flush(6)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+  enddo
+
+  elemRankCounter = 0
+  ! Determine number of neighbors
+  do iz = gridIndicesStart(3) - 1, gridIndicesEnd(3) + 1
+    do iy = gridIndicesStart(2) - 1, gridIndicesEnd(2) + 1
+      do ix = gridIndicesStart(1) - 1, gridIndicesEnd(1) + 1
+        elemRank = getElemRank(ix,iy,iz)
+        if (elemRank .ne. -1) then
+          tmpDir = 0
+          elemRankCounter(elemRank) = 1
+
+        endif
+      enddo
+    enddo
+  enddo
+
+  rankCounter = 0
+  do iRank = 0, numProcs - 1
+    if (elemRankCounter(iRank) .gt. 0) then
+      rankCounter = rankCounter + 1
+    endif
+  enddo
+
+  ! Subtract one for self
+  rankCounter = rankCounter - 1
+  numMessages = rankCounter
+
+  allocate(neighborRanks(numMessages))
+  ! Determine neighbor ranks
+  msgIndices = 0
+  rankCounter = 0
+  do iz = gridIndicesStart(3) - 1, gridIndicesEnd(3) + 1
+    do iy = gridIndicesStart(2) - 1, gridIndicesEnd(2) + 1
+      do ix = gridIndicesStart(1) - 1, gridIndicesEnd(1) + 1
+        elemRank = getElemRank(ix,iy,iz)
+        if (elemRank .ne. -1 .and. elemRank .ne. rank) then
+          found = .false.
+          do iRank = 1, rankCounter
+            if (neighborRanks(iRank) .eq. elemRank) then
+              found = .true.
+              exit
+            endif
+          enddo
+          if (.not. found) then
+            rankCounter = rankCounter + 1
+            neighborRanks(rankCounter) = elemRank 
+            tmpDirZ = 0
+            tmpDirY = 0
+            tmpDirX = 0
+            if (iz .gt. gridIndicesEnd(3)) then
+              tmpDirZ = 1
+            else if (iz .lt. gridIndicesStart(3)) then
+              tmpDirZ = -1
+            endif
+            if (iy .gt. gridIndicesEnd(2)) then
+              tmpDirY = 1
+            else if (iy .lt. gridIndicesStart(2)) then
+              tmpDirY = -1
+            endif
+            if (ix .gt. gridIndicesEnd(1)) then
+              tmpDirX = 1
+            else if (iz .gt. gridIndicesStart(1)) then
+              tmpDirX = -1
+            endif
+            msgIndices(tmpDirX,tmpDirY,tmpDirZ) = rankCounter
+            
+
+          endif
+        endif
+      enddo
+    enddo
+  enddo
+
+  numMessages = numMessages
+  allocate(msgSizes(numMessages))
+  allocate(msgOffsets(numMessages+1))
+  msgSizes = 0
+  ! Determine message sizes
+  do iz = gridIndicesStart(3) - 1, gridIndicesEnd(3) + 1
+    do iy = gridIndicesStart(2) - 1, gridIndicesEnd(2) + 1
+      do ix = gridIndicesStart(1) - 1, gridIndicesEnd(1) + 1
+        elemRank = getElemRank(ix,iy,iz)
+        if (elemRank .ne. -1 .and. elemRank .ne. rank) then
+          do iRank = 1, numMessages
+            if (neighborRanks(iRank) .eq. elemRank) then
+              msgSizes(iRank) = msgSizes(iRank) + 1
+              exit
+            endif
+          enddo
+        endif
+      enddo
+    enddo
+  enddo
+
+  totalMessageSize = 0
+  do i = 1, numMessages
+    msgOffsets(i) = totalMessageSize
+    totalMessageSize = totalMessageSize + msgSizes(i)
+  enddo
+  msgOffsets(numMessages+1) = totalMessageSize
+
+  do iRank = 0, numProcs - 1
+    if (iRank == rank) then
+      print *, "Rank ", rank, " has ", numMessages, " neighbors"
+      do i = 1, numMessages
+        print *, "  pair Rank ", neighborRanks(i), ", size ", msgSizes(i)
+      enddo
+      print *, "  totalSize ", totalMessageSize
+    endif
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call flush(6)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+    call MPI_Barrier(MPI_COMM_WORLD, ierr)
+  enddo
+
+  end subroutine
 
 end module 
 
--- test/poisson3d.F90
+++ test/poisson3d.F90
@@ -5,6 +5,467 @@ module arrayOfPointers_mod
 end module 
 
 
+module commCycle
+
+contains
+
+function evalOperator(inData, ix, iy, iz) result(outData)
+implicit none
+real*8, intent(in) :: inData(:,:,:)
+integer, intent(in) :: ix, iy, iz
+
+real*8 :: outData
+
+integer :: i, j, k
+real*8 :: coefs(-1:1,-1:1,-1:1)
+
+! TODO change this to 27 point stencil
+coefs(0,0,-1) = 1
+coefs(0,-1,0) = 1
+coefs(-1,0,0) = 1
+coefs(0,0,0) = -8
+coefs(1,0,0) = 1
+coefs(0,1,0) = 1
+coefs(0,0,1) = 1
+outData = 0
+do k = -1, 1
+  do j = -1, 1
+    do i = -1, 1
+      outData = outData + coefs(i,j,k)*inData(i,j,k)
+    enddo
+  enddo
+enddo
+
+end function
+
+
+subroutine haloExchange
+
+end subroutine
+
+subroutine relaxation(inData, outData, gridIndicesStart, gridIndicesEnd)
+implicit none
+real*8, intent(out) :: outData(:,:,:)
+integer, intent(in) :: gridIndicesStart(3), gridIndicesEnd(3)
+real*8, intent(in) :: inData(gridIndicesStart(1)-1:gridIndicesEnd(1)+1, &
+                             gridIndicesStart(2)-1:gridIndicesEnd(2)+1, &
+                             gridIndicesStart(3)-1:gridIndicesEnd(3)+1)
+
+integer :: ix, iy, iz
+
+do iz = gridIndicesStart(3), gridIndicesEnd(3)
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      outData(ix, iy, iz) = evalOperator(inData, ix, iy, iz)
+    enddo
+  enddo
+enddo
+end subroutine
+
+subroutine packData(inData, gridIndicesStart, gridIndicesEnd,  &
+                    msgOffsets, msgLengths, packBuffer, packDir)
+integer, intent(in) :: gridIndicesStart(3), gridIndicesEnd(3)
+real*8, intent(in) :: inData(gridIndicesStart(1)-1:gridIndicesEnd(1)+1, &
+                             gridIndicesStart(2)-1:gridIndicesEnd(2)+1, &
+                             gridIndicesStart(3)-1:gridIndicesEnd(3)+1)
+integer, intent(in) :: msgOffsets(:), msgLengths(:)
+real*8, intent(out) :: packBuffer(:)
+integer, intent(in) :: packDir(-1:1,-1:1,-1:1)
+
+
+integer :: packIndex, packOffset
+
+! pack top
+packIndex = packDir(0,0,1)
+if (packIndex > 0) then
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+
+! pack bottom
+packIndex = packDir(0,0,-1)
+if (packIndex > 0) then
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+
+! pack south
+packIndex = packDir(0,-1,0)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack north
+packIndex = packDir(0,1,0)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack east
+packIndex = packDir(-1,0,0)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do iy = gridIndicesStart(2), gridIndicesEnd(2)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack west
+packIndex = packDir(1,0,0)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do iy = gridIndicesStart(2), gridIndicesEnd(2)
+      packBuffer(packOffset) = inData(ix,iy,iz)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! Pack bottom edges
+! Bottom east
+packIndex = packDir(-1,0,-1)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom west
+packIndex = packDir(1,0,-1)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom south
+packIndex = packDir(0,-1,-1)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom north
+packIndex = packDir(0,1,-1)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+
+! Pack top edges
+! Bottom east
+packIndex = packDir(-1,0,1)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom west
+packIndex = packDir(1,0,1)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom south
+packIndex = packDir(0,-1,1)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom north
+packIndex = packDir(0,1,1)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    packBuffer(packOffset) = inData(ix,iy,iz)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+end subroutine
+
+subroutine unpackData(inData, gridIndicesStart, gridIndicesEnd,  &
+                    msgOffsets, msgLengths, packBuffer, packDir)
+integer, intent(in) :: gridIndicesStart(3), gridIndicesEnd(3)
+real*8, intent(inout) :: inData(gridIndicesStart(1)-1:gridIndicesEnd(1)+1, &
+                                gridIndicesStart(2)-1:gridIndicesEnd(2)+1, &
+                               gridIndicesStart(3)-1:gridIndicesEnd(3)+1)
+integer, intent(in) :: msgOffsets(:), msgLengths(:)
+real*8, intent(in) :: packBuffer(:)
+integer, intent(in) :: packDir(-1:1,-1:1,-1:1)
+
+
+integer :: packIndex, packOffset
+
+! pack top
+packIndex = packDir(0,0,1)
+if (packIndex > 0) then
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+
+! pack bottom
+packIndex = packDir(0,0,-1)
+if (packIndex > 0) then
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+
+! pack south
+packIndex = packDir(0,-1,0)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack north
+packIndex = packDir(0,1,0)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do ix = gridIndicesStart(1), gridIndicesEnd(1)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack east
+packIndex = packDir(-1,0,0)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do iy = gridIndicesStart(2), gridIndicesEnd(2)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! pack west
+packIndex = packDir(1,0,0)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iz = gridIndicesStart(3), gridIndicesEnd(3)
+    do iy = gridIndicesStart(2), gridIndicesEnd(2)
+      !packBuffer(packOffset) = inData(ix,iy,iz)
+      inData(ix,iy,iz) = packBuffer(packOffset)
+      packOffset = packOffset + 1
+    enddo
+  enddo
+endif
+
+! Pack bottom edges
+! Bottom east
+packIndex = packDir(-1,0,-1)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom west
+packIndex = packDir(1,0,-1)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom south
+packIndex = packDir(0,-1,-1)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom north
+packIndex = packDir(0,1,-1)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  iz = gridIndicesStart(3) - 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+
+! Pack top edges
+! Bottom east
+packIndex = packDir(-1,0,1)
+if (packIndex > 0) then
+  ix = gridIndicesStart(1) - 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom west
+packIndex = packDir(1,0,1)
+if (packIndex > 0) then
+  ix = gridIndicesEnd(1) + 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do iy = gridIndicesStart(2), gridIndicesEnd(2)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom south
+packIndex = packDir(0,-1,1)
+if (packIndex > 0) then
+  iy = gridIndicesStart(2) - 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+! Bottom north
+packIndex = packDir(0,1,1)
+if (packIndex > 0) then
+  iy = gridIndicesEnd(2) + 1
+  iz = gridIndicesEnd(3) + 1
+  packOffset = msgOffsets(packIndex) + 1
+  do ix = gridIndicesStart(1), gridIndicesEnd(1)
+    !packBuffer(packOffset) = inData(ix,iy,iz)
+    inData(ix,iy,iz) = packBuffer(packOffset)
+    packOffset = packOffset + 1
+  enddo
+endif
+
+end subroutine
+
+end module commCycle
+
 program nodalCommFtn
 #include "Mashmf.h"
 use iso_c_binding
@@ -12,13 +473,26 @@ use Mashm_mod
 use mpi
 use arrayOfPointers_mod
 use grid_data
+use commCycle
 implicit none
 
 integer :: ierr
 integer :: rank, numProcs
 integer :: numElems
-integer, allocatable :: gridIndices(:,:)
-
+integer, pointer :: gridIndicesStart(:), gridIndicesEnd(:)
+integer :: ix, iy, iz, iRank
+integer :: elemRank, rankCounter
+integer, allocatable :: elemRankCounter(:)
+integer :: numMessages
+integer, allocatable :: neighborRanks(:), msgSizes(:)
+logical :: found
+real*8, allocatable :: domain(:,:,:)
+real*8, allocatable :: outData(:,:,:)
+integer :: totalMessageSize
+real*8, allocatable :: packBuffer(:)
+integer, allocatable :: msgOffsets(:)
+integer :: offsetValue, i, j, k
+integer :: msgDirIndex(-1:1,-1:1,-1:1)
 Mashm :: myMashm
 
 call MPI_Init(ierr)
@@ -32,17 +506,47 @@ call read_grid_data_namelist(MPI_COMM_WORLD)
 ! Get the grid decomposition
 call grid_3d_decomp_num_elements(rank, numProcs)
 
-!allocate(gridIndices(3,numElems))
-
-! 
-!call grid_3d_decomp_get_elements(rank, numProcs)
+call grid_3d_get_indices(numElems, gridIndicesStart, gridIndicesEnd)
 
 
+call determineCommSchedule(rank, numProcs, gridIndicesStart, gridIndicesEnd, numMessages, msgDirIndex, &
+                           msgSizes, msgOffsets)
+totalMessageSize=msgOffsets(numMessages+1)
+allocate(packBuffer(totalMessageSize))
 
-!deallocate(gridIndices)
+! Initialize domain data
+allocate(domain(gridIndicesStart(1)-1:gridIndicesEnd(1)+1, &
+                gridIndicesStart(2)-1:gridIndicesEnd(2)+1, &
+                gridIndicesStart(3)-1:gridIndicesEnd(3)+1))
+
+! Initialize to zero
+domain = 0
+offsetValue = rank &
+            * (gridIndicesEnd(3)-gridIndicesStart(3)+2) &
+            * (gridIndicesEnd(2)-gridIndicesStart(2)+2) &
+            * (gridIndicesEnd(1)-gridIndicesStart(1)+2)
+do k = gridIndicesStart(3)-1, gridIndicesEnd(3)+1
+  do j = gridIndicesStart(2)-1, gridIndicesEnd(2)+1
+    do i = gridIndicesStart(1)-1, gridIndicesEnd(1)+1
+      domain(i,j,k) = offsetValue &
+                      + k*(gridIndicesEnd(2)-gridIndicesStart(2)+2)*(gridIndicesEnd(1)-gridIndicesStart(1)+2) &
+                      + j*(gridIndicesEnd(1)-gridIndicesStart(1)+2) + i
+    enddo
+  enddo
+enddo
+
+call packData(domain, gridIndicesStart, gridIndicesEnd,  &
+              msgOffsets, msgSizes, packBuffer, msgDirIndex)
+
+! Need messaging loop
+!  Should have all of the data that we need
+!  Allocate a receive buffer - pass that into unpack
+
+call unpackData(domain, gridIndicesStart, gridIndicesEnd,  &
+              msgOffsets, msgSizes, packBuffer, msgDirIndex)
+
 
 call MPI_Finalize(ierr)
 
 end program nodalCommFtn
-
 
